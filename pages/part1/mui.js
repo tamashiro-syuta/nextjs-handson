25d4d44087f5cde4114827dce847c799c9199c82
tamashiro-syuta <syuta5825@gmail.com>
2022-02-11 09:55:10 +0900

    emotionを使ったCSSの当て方
    nextjsではSSRの関係でemotionが推奨されている。
    
    cssの当て方の検討順番
    ①コンポーネントのプロパティ https://mui.com/api/button/
    ②sxプロパティ https://mui.com/system/the-sx-prop/
    ③emotion https://mui.com/guides/interoperability/#emotion

--- pages/part1/mui.js
+++ pages/part1/mui.js
@@ -1,13 +1,36 @@
 import React from 'react';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
+const style = {
+  //container用のスタイルを定義
+  container: css`
+    background-color: red;
+  `,
+  // box用のスタイルを定義
+  box: css`
+    background-color: blue;
+  `,
+};
+
 const Mui = () => {
   return (
-    <Container component="main" maxWidth="xs">
-      <Box component="form" noValidate sx={{ marginTop: 8 }}>
+    <Container component="main" maxWidth="xs" css={style.container}>
+      <Box
+        component="form"
+        noValidate
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+        css={style.box}
+      >
         <TextField label="キーワードを入力してください" variant="standard" margin="normal" fullWidth />
         <Button variant="contained" margin="normal" fullWidth>
           検索

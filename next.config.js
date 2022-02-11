93222cf40d08e7d56296acd78a01ba9fa4b34155
tamashiro-syuta <syuta5825@gmail.com>
2022-02-11 11:05:02 +0900

    設定した環境変数をnextが読み込むように設定

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
+// next.js側に環境変数を取り込む設定
+
+// VERCEL(デプロイで使うURL)とHOTPEPPER_API_KEY(APIキー)を定義し、.envから取得
+const { VERCEL_URL, HOTPEPPER_API_KEY } = process.env;
+
 module.exports = {
   reactStrictMode: true,
-}
+  // publicRuntimeConfig --> 基本的に漏れても良い設定値(ブラウザ側で利用するので漏れる)
+  publicRuntimeConfig: {
+    // VERCELのurlがあれば、そちらのurlに通信し、なければローカルに通信する
+    API_HOST: VERCEL_URL ? `https://${VERCEL_URL}` : 'http://localhost:3000',
+  },
+  // serverRuntimeConfig --> 漏れてはいけないサーバー側で利用する値(APIキーなど)
+  serverRuntimeConfig: {
+    HOTPEPPER_API_KEY,
+  },
+};
